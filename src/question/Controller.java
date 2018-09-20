9ac950af25324d69174ffe4e76183987aeb6ab47
Marcoliuzzi <43071141+Marcoliuzzi@users.noreply.github.com>
2018-09-20 08:27:16 -0600

    added java dot

--- src/question/Controller.java
+++ src/question/Controller.java
@@ -1,6 +1,28 @@
 package question;
 
+//Import Section
+import model.BobTheTrex;
+import javax.swing.JOptionPane;
+
 public class Controller
 {
+	/**
+	* Builds the instance of the Controller.
+	*/
+		
+	public Controller()
+	{
+			
+	}
+	
+	/**
+	 * This is where the program starts calling methods to run the program.
+	 */
+	public void start()
+	{
+		
+	}
+	
+	
 
 }
